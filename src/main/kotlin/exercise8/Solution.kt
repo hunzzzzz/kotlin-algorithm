ab12e07d3b703cab5a00de9639279eba54e2a113
hunzz <hunzz.study@gmail.com>
2023-12-04 16:53:54 +0900

    exercise 8, exercise 9

---
+++ src/main/kotlin/exercise8/Solution.kt
@@ -0,0 +1,13 @@
+package exercise8
+
+class Solution {
+    fun solution(angle: Int): Int {
+        return when (angle) {
+            in 1..89 -> 1
+            90 -> 2
+            in 91..179 -> 3
+            180 -> 4
+            else -> -1
+        }
+    }
+}
---
+++ src/main/kotlin/exercise9/Solution.kt
@@ -0,0 +1,12 @@
+package exercise9
+
+class Solution {
+    fun solution(n: Int): Int {
+        var sum = 0
+        for (i in 1..n) {
+            if (i % 2 == 0)
+                sum += i
+        }
+        return sum
+    }
+}
