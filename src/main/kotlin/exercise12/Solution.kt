bce98b8bb7bbe0e8db34ef0ce8cb8b7eb2c250de
hunzz <hunzz.study@gmail.com>
2023-12-04 17:05:50 +0900

    exercise 12

---
+++ src/main/kotlin/exercise12/Solution.kt
@@ -0,0 +1,12 @@
+package exercise12
+
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var sum = 0.0
+
+        for (num in arr)
+            sum += num
+
+        return sum / arr.size
+    }
+}
