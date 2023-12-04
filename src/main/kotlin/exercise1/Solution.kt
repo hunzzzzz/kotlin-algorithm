d6ad2e0ca36a0f2c3111f063acd7e2cac59cb501
hunzz <hunzz.study@gmail.com>
2023-12-04 12:19:44 +0900

    exercise 1, exercise 2 modified

--- src/main/kotlin/exercise1/Solution.kt
+++ src/main/kotlin/exercise1/Solution.kt
@@ -2,7 +2,6 @@ package exercise1
 
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
-        val answer: Int = num1 - num2
-        return answer
+        return num1 - num2
     }
 }
