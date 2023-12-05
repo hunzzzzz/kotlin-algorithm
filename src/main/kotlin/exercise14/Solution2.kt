f3e75076b2cea5deaa70baa6b1947e0107603b06
hunzz <hunzz.study@gmail.com>
2023-12-05 09:44:13 +0900

    exercise 14 Solution2 modified, exercise 15

--- src/main/kotlin/exercise14/Solution2.kt
+++ src/main/kotlin/exercise14/Solution2.kt
@@ -1,7 +1,5 @@
 package exercise14
 
 class Solution2 {
-    fun solution(n: Int): Int {
-        return (1..n / 2).filter { n % it == 0 }.sum() + n
-    }
+    fun solution(n: Int) = (1..n / 2).filter { n % it == 0 }.sum() + n
 }
