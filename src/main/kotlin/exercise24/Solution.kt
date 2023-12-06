7c9447f58811bfaf60ab497e40eaa3c4f8679044
hunzz <hunzz.study@gmail.com>
2023-12-06 09:31:25 +0900

    exercise 24, exercise 25

---
+++ src/main/kotlin/exercise24/Solution.kt
@@ -0,0 +1,10 @@
+package exercise24
+
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        for (i in seoul.indices)
+            if (seoul[i] == "Kim")
+                return "김서방은 ${i}에 있다"
+        return ""
+    }
+}
---
+++ src/main/kotlin/exercise25/Solution.kt
@@ -0,0 +1,29 @@
+package exercise25
+
+class Solution {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        var answer = intArrayOf()
+        for (num in arr)
+            if (num % divisor == 0)
+                answer = answer.plus(num)
+        bubbleSort(answer)
+
+        if (answer.isEmpty())
+            answer = answer.plus(-1)
+
+        return answer
+    }
+
+    private fun bubbleSort(arr: IntArray) {
+        var temp = 0
+        for (i in arr.indices) {
+            for (j in 0 until arr.size - 1 - i) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                }
+            }
+        }
+    }
+}
