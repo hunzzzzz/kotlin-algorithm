e4ac9d4da81b46a7f01613dd72a6e03d3ab453cd
hunzz <hunzz.study@gmail.com>
2023-12-07 16:19:32 +0900

    exercise 35

---
+++ src/main/kotlin/exercise35/Solution.kt
@@ -0,0 +1,13 @@
+package exercise35
+
+class Solution {
+    fun solution(price: Int, money: Int, count: Int): Long {
+        var nestedFee = 0L
+        for (i in 1..count)
+            nestedFee += price * i
+
+        val shortage = nestedFee - money.toLong()
+
+        return if (shortage < 0) 0 else shortage
+    }
+}
