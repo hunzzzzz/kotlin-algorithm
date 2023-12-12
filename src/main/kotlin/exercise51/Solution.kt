f16dfaca3d8f49bd0c710d32448b989baaf0da30
hunzz <hunzz.study@gmail.com>
2023-12-12 23:42:17 +0900

    exercise 51

---
+++ src/main/kotlin/exercise51/Solution.kt
@@ -0,0 +1,14 @@
+package exercise51
+
+class Solution {
+    fun solution(food: IntArray): String {
+        val answer = StringBuilder()
+        for (i in 1..food.size - 1)
+            answer.append(i.toString().repeat(food[i] / 2))
+        answer.append("0")
+        for (i in food.size - 1 downTo 1)
+            answer.append(i.toString().repeat(food[i] / 2))
+
+        return answer.toString()
+    }
+}
