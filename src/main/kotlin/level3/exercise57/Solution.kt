d7b9f0792df1853d4e75618f7bf4477de84095d1
hunzz <hunzz.study@gmail.com>
2023-12-20 20:07:05 +0900

    modify exercise 57

--- src/main/kotlin/level3/exercise57/Solution.kt
+++ src/main/kotlin/level3/exercise57/Solution.kt
@@ -2,11 +2,12 @@ package level3.exercise57
 
 class Member(index: Int, list: MutableList<Int>) {
     var memberIndex: Int
-    var memberScore = 0
-    private var pattern = mutableListOf<Int>()
+    var memberScore: Int
+    private var pattern: MutableList<Int>
 
     init {
         this.memberIndex = index
+        this.memberScore = 0
         this.pattern = list
     }
 
@@ -29,22 +30,21 @@ class Solution {
         val member1 = Member(1, mutableListOf(1, 2, 3, 4, 5))
         val member2 = Member(2, mutableListOf(2, 1, 2, 3, 2, 4, 2, 5))
         val member3 = Member(3, mutableListOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5))
-        val memberList = mutableListOf<Member>(member1, member2, member3)
+        val memberList = mutableListOf(member1, member2, member3)
+
+        val member1Pattern = member1.repeatPattern(answers.size)
+        val member2Pattern = member2.repeatPattern(answers.size)
+        val member3Pattern = member3.repeatPattern(answers.size)
 
         for (i in answers.indices) {
-            if (answers[i] == member1.repeatPattern(answers.size)[i]) member1.memberScore++
-            if (answers[i] == member2.repeatPattern(answers.size)[i]) member2.memberScore++
-            if (answers[i] == member3.repeatPattern(answers.size)[i]) member3.memberScore++
+            if (answers[i] == member1Pattern[i]) member1.memberScore++
+            if (answers[i] == member2Pattern[i]) member2.memberScore++
+            if (answers[i] == member3Pattern[i]) member3.memberScore++
         }
 
-        var answer = intArrayOf()
         val maxScore = memberList.maxOf { it.memberScore }
+        val answer = memberList.filter { it.memberScore == maxScore }.map { it.memberIndex }
 
-        for (member in memberList) {
-            if (member.memberScore == maxScore)
-                answer = answer.plus(member.memberIndex)
-        }
-
-        return answer
+        return answer.toIntArray()
     }
 }
